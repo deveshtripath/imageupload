46e5de808c5663884b37cf1dd1b9c7e78c104dc6
devesh <devehtripathi2020@gmail.com>
2021-07-31 13:55:59 +0530

    deploy

--- imageuploader/settings.py
+++ imageuploader/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
